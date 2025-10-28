c048e1b0232b358317c620db68597a6b57e64d12
wlstjd1209 <rtgvyu@gmail.com>
2025-10-28 11:37:02 +0900

    Update title.py

--- title.py
+++ title.py
@@ -39,104 +39,104 @@ if wordinput:  # "" -> False, "bla-bla~" -> True
         
         for num, data in enumerate(data1, start = 1):
             "---"
-            with st.container():
-                st.write(num)
-                i+=1
-    
-                for mean in data["meanings"]:
-                    
-                    if "partOfSpeech" in mean:
-                        meanpos = mean["partOfSpeech"]
-    
-                        if meanpos == "noun":
-                            st.subheader(f":orange[**명사**]")
-    
-                        elif meanpos == "pronoun":
-                            st.subheader(f":orange[**대명사**]")
-    
-                        elif meanpos == "verb":
-                            st.subheader(f":orange[**동사**]")
-    
-                        elif meanpos == "adjective":
-                            st.subheader(f":orange[**형용사**]")
-    
-                        elif meanpos == "adverb":
-                            st.subheader(f":orange[**부사**]")   
-    
-                        elif meanpos == "preposition":
-                            st.subheader(f":orange[**전치사**]")
-                            
-                        elif meanpos == "conjunction":
-                            st.subheader(f":orange[**접속사**]")
-    
-                        elif meanpos == "interjection":
-                            st.subheader(f":orange[**감탄사**]")
-    
-                        else:
-                            st.subheader(f":orange[**{meanpos}**]")
-                    
-                    with st.expander("🔽 정의&예문"):
-                        kkk=0
-                        if "definitions" in mean:
-    
-                            for wjddmlemf in mean["definitions"]:
-                                kkk+=1
-                                if "definition" in wjddmlemf:
-                                    meandef = wjddmlemf["definition"]
-    
-                                    if "example" in wjddmlemf:
-                                        meanex = wjddmlemf["example"]
-                                    else:
-                                        meanex = "NOT FOUND"
-                                    st.markdown(f"{kkk}. :orange[{meandef}]")
-                                    if meanex == "NOT FOUND":
-                                        st.caption("EXAMPLE IS NOT PROVIDED")
-                                    else:
-                                        st.caption(f"ex) {meanex}")
-                                                
-                    with st.expander("🔽 유의어/동의어"):
-                        if "synonyms" in mean:
-                            
-                            dbdmldjemf = mean["synonyms"]
-    
-                            if dbdmldjemf == []:
-                                    st.caption(f"SYNONYM IS NOT FOUND")
-                            else:
-                                for kkkk, meansyn in enumerate(dbdmldjemf, start = 1):
-                                    st.markdown(f"{kkkk}. :orange[{meansyn}]")
-                        else:
-                            st.caption(f"SYNONYM IS NOT FOUND")
-    
-                    with st.expander("🔽 반의어"):
-                        if "antonyms" in mean:
-                            
-                            qksdmldjemf = mean["antonyms"]
-    
-                            if qksdmldjemf == []:
-                                    st.caption(f"ANTONYM IS NOT FOUND")
-                            else:
-                                for kkkkk, meanant in enumerate(qksdmldjemf, start = 1):
-                                    st.markdown(f"{kkkkk}. :orange[{meanant}]")
-                        else:
-                            st.caption(f"ANTONYM IS NOT FOUND")
-    
-                for pho in data["phonetics"]:
-                    if "text" in pho:
-                        textpho = pho["text"]
-                        if "audio" in pho:
-                            audiopho = pho["audio"]
-                            if audiopho == "":
-                                continue
-                            else:
-                                audioresponse = requests.get(audiopho)
-    
-                                if audiopho[-5] == "s":
-                                    st.write(f"**🔊발음 : {textpho}**  :green[*(US)*]")
-                                elif audiopho[-5] == "k":
-                                    st.write(f"**🔊발음 : {textpho}**  :green[*(UK)*]")
+        
+            st.write(num)
+            i+=1
+
+            for mean in data["meanings"]:
+                
+                if "partOfSpeech" in mean:
+                    meanpos = mean["partOfSpeech"]
+
+                    if meanpos == "noun":
+                        st.subheader(f":orange[**명사**]")
+
+                    elif meanpos == "pronoun":
+                        st.subheader(f":orange[**대명사**]")
+
+                    elif meanpos == "verb":
+                        st.subheader(f":orange[**동사**]")
+
+                    elif meanpos == "adjective":
+                        st.subheader(f":orange[**형용사**]")
+
+                    elif meanpos == "adverb":
+                        st.subheader(f":orange[**부사**]")   
+
+                    elif meanpos == "preposition":
+                        st.subheader(f":orange[**전치사**]")
+                        
+                    elif meanpos == "conjunction":
+                        st.subheader(f":orange[**접속사**]")
+
+                    elif meanpos == "interjection":
+                        st.subheader(f":orange[**감탄사**]")
+
+                    else:
+                        st.subheader(f":orange[**{meanpos}**]")
+                
+                with st.expander("🔽 정의&예문"):
+                    kkk=0
+                    if "definitions" in mean:
+
+                        for wjddmlemf in mean["definitions"]:
+                            kkk+=1
+                            if "definition" in wjddmlemf:
+                                meandef = wjddmlemf["definition"]
+
+                                if "example" in wjddmlemf:
+                                    meanex = wjddmlemf["example"]
                                 else:
-                                    continue
-                                st.audio(audioresponse.content, format = "audio/mp3")
+                                    meanex = "NOT FOUND"
+                                st.markdown(f"{kkk}. :orange[{meandef}]")
+                                if meanex == "NOT FOUND":
+                                    st.caption("EXAMPLE IS NOT PROVIDED")
+                                else:
+                                    st.caption(f"ex) {meanex}")
+                                            
+                with st.expander("🔽 유의어/동의어"):
+                    if "synonyms" in mean:
+                        
+                        dbdmldjemf = mean["synonyms"]
+
+                        if dbdmldjemf == []:
+                                st.caption(f"SYNONYM IS NOT FOUND")
+                        else:
+                            for kkkk, meansyn in enumerate(dbdmldjemf, start = 1):
+                                st.markdown(f"{kkkk}. :orange[{meansyn}]")
+                    else:
+                        st.caption(f"SYNONYM IS NOT FOUND")
+
+                with st.expander("🔽 반의어"):
+                    if "antonyms" in mean:
+                        
+                        qksdmldjemf = mean["antonyms"]
+
+                        if qksdmldjemf == []:
+                                st.caption(f"ANTONYM IS NOT FOUND")
+                        else:
+                            for kkkkk, meanant in enumerate(qksdmldjemf, start = 1):
+                                st.markdown(f"{kkkkk}. :orange[{meanant}]")
+                    else:
+                        st.caption(f"ANTONYM IS NOT FOUND")
+
+            for pho in data["phonetics"]:
+                if "text" in pho:
+                    textpho = pho["text"]
+                    if "audio" in pho:
+                        audiopho = pho["audio"]
+                        if audiopho == "":
+                            continue
+                        else:
+                            audioresponse = requests.get(audiopho)
+
+                            if audiopho[-5] == "s":
+                                st.write(f"**🔊발음 : {textpho}**  :green[*(US)*]")
+                            elif audiopho[-5] == "k":
+                                st.write(f"**🔊발음 : {textpho}**  :green[*(UK)*]")
+                            else:
+                                continue
+                            st.audio(audioresponse.content, format = "audio/mp3")
 
 
     else:
@@ -145,6 +145,7 @@ else:
     st.warning("단어를 입력해주세요.")
 
 st.rerun()
+
 
 
 
