db98623048db62ad8e11e50051308419991006cc
wlstjd1209 <rtgvyu@gmail.com>
2025-11-03 07:10:30 +0900

    Update title.py

--- title.py
+++ title.py
@@ -2,37 +2,86 @@ import streamlit as st
 import requests
 
 st.title(":violet[영어 단어 사전]")
-
+# 
 if "history" not in st.session_state:
     st.session_state["history"] = []
 if "word" not in st.session_state:
     st.session_state["word"] = ""
+
+def addhistory():
+    if st.session_state["word"] in st.session_state["history"]:
+        st.session_state["history"].remove(st.session_state["word"])
+        st.session_state["history"].insert(0, st.session_state["word"])
+    elif st.session_state["word"] not in st.session_state["history"]:
+        st.session_state["history"].insert(0, st.session_state["word"])
+
+    # 10개 제한
+    if len(st.session_state["history"]) > 10:
+        st.session_state["history"].pop(-1)
     
-col1, col2 = st.columns([3,1])
+def historyinput():
+    st.session_state["word"]
+
+# def set_word():
+
+col1, col2, col3, col4 = st.columns([5,1,2,1])
 
 with col1:
-    wordinput = st.text_input("영어 단어 입력:", st.session_state["word"])
+    # wordinput 받고 st.session_state["word"]에 저장
+    wordinput = st.text_input(
+        "영어 단어 입력:",
+        key="word"
+        
+        )
+#만약 wordinput받으면 기록추가
+if wordinput:
+    addhistory()
 
 with col2:
+    st.write("")
+    st.write("")
+    button1 = st.button("검색", key="button1")
+    if button1:
+        st.rerun()
+
+with col3:
+    
     if st.session_state["history"]:
-        selected = st.selectbox("검색기록", st.session_state["history"], index = None, placeholder = "선택하세요")
-        if selected and selected != st.session_state["word"]:
-            st.session_state["word"] = selected
-            st.experimental_rerun()
+        options = ["---선택---"] + st.session_state["history"]
+        selected = st.selectbox("검색기록", options, index=0)
     else:
-        st.info("검색 기록X")
-    
-    
-if wordinput:  # "" -> False, "bla-bla~" -> True
-    if st.session_state["word"] not in st.session_state["history"]:
-        st.session_state["history"].insert(0, wordinput)
+        st.caption("검색기록없음")
 
     
+
+
+with col4:
+    if st.session_state["history"]:
+        st.write("")
+        st.write("")
+        button2 = st.button("검색", key="button2")
+        if button2:
+            wordinput = selected
+            # st.rerun()
+            # addhistory()
+
+
+
+if wordinput:  # "" -> False, "bla-bla~" -> True
     url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{wordinput}"
+
     response = requests.get(url)
+
+    # st.write(response.status_code)
     data1 = response.json()
+    # trial = response.json()[0]
+    # st.write(data1)
+    # st.write(trial)
+
+
 
     i=0
+
 
     upcountry = ["UK", "US"]
     lowcountry = ["uk", "us"]
@@ -42,9 +91,9 @@ if wordinput:  # "" -> False, "bla-bla~" -> True
         
         for num, data in enumerate(data1, start = 1):
             "---"
-        
             st.write(num)
             i+=1
+            
 
             for mean in data["meanings"]:
                 
@@ -93,7 +142,7 @@ if wordinput:  # "" -> False, "bla-bla~" -> True
                                     meanex = "NOT FOUND"
                                 st.markdown(f"{kkk}. :orange[{meandef}]")
                                 if meanex == "NOT FOUND":
-                                    st.caption("EXAMPLE IS NOT PROVIDED")
+                                    st.caption("EXAMPLE IS NOT FOUND")
                                 else:
                                     st.caption(f"ex) {meanex}")
                                             
@@ -110,6 +159,7 @@ if wordinput:  # "" -> False, "bla-bla~" -> True
                     else:
                         st.caption(f"SYNONYM IS NOT FOUND")
 
+
                 with st.expander("🔽 반의어"):
                     if "antonyms" in mean:
                         
@@ -122,6 +172,32 @@ if wordinput:  # "" -> False, "bla-bla~" -> True
                                 st.markdown(f"{kkkkk}. :orange[{meanant}]")
                     else:
                         st.caption(f"ANTONYM IS NOT FOUND")
+
+                # if "antonyms" in mean:
+                #     st.subheader(f"**반의어 :**")
+                    
+                #     meanann = mean["antonyms"]
+
+                #     st.json(meanann)
+
+
+
+                        
+            # for pho in data["phonetics"]:
+            #         if "text" in pho:
+            #             textpho = pho["text"]
+                        
+            #             st.write(f"**발음 : {textpho}**  :green[*({upcountry[k]})*]")
+            #             k+=1
+            #         else:
+            #             st.write(f"*:red[Text is Not Found]*")
+
+            #         if "audio" in pho:
+            #             audiopho = pho["audio"]
+            #             audioresponse = requests.get(audiopho)
+            #             st.audio(audioresponse.content, format = "audio/mp3")
+            #         else:
+            #             st.write(f"*:red[Audio is Not Found]*")
 
             for pho in data["phonetics"]:
                 if "text" in pho:
@@ -142,9 +218,28 @@ if wordinput:  # "" -> False, "bla-bla~" -> True
                             st.audio(audioresponse.content, format = "audio/mp3")
 
 
+
+
+            # for pho in data["phonetics"]:
+            #         if "text" in pho:
+            #             textpho = pho["text"]
+                        # if "audio" in pho:
+                        #     audiopho = pho["audio"]
+                        #     audioresponse = requests.get(audiopho)
+
+                        #     if audiopho[-5] == "s":
+                        #         button = st.button(f"**🔊발음 : {textpho}**  :green[*(US)*]")
+                        #     elif audiopho[-5] == "k":
+                        #         button = st.button(f"**🔊발음 : {textpho}**  :green[*(UK)*]")
+                        #     else:
+                        #         continue
+
+
+            #                 if button:
+            #                     st.audio(audioresponse.content, format = "audio/mp3")
+
+
     else:
         st.error(f"""단어 정보를 가져오는 데 실패했습니다. (상태 코드 : **{response.status_code}**)""")
 else:
     st.warning("단어를 입력해주세요.")
-
-
