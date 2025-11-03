01761b86f61efada12724b0ca0e2c8b1457eb24e
wlstjd1209 <rtgvyu@gmail.com>
2025-11-03 23:34:46 +0900

    Update title.py

--- title.py
+++ title.py
@@ -106,7 +106,7 @@ if wordinput:  # "" -> False, "bla-bla~" -> True
         
         for num, data in enumerate(data1, start = 1):
             "---"
-            st.write(num)
+            st.info(num)
             i+=1
             
 
@@ -258,4 +258,5 @@ if wordinput:  # "" -> False, "bla-bla~" -> True
         st.error(f"""단어 정보를 가져오는 데 실패했습니다💦 (상태 코드 : **{response.status_code}**)""")
 else:
     st.warning("단어를 입력해주세요.")
+
 
